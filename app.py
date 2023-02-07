58a2fe3957c0761dba35d770bcb6f2e40db2e9da
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2023-02-07 16:44:55 -0500

    Make HTML form that submits to server

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+
+@app.route('/generate_recipes', methods=['POST'])
+def generate_recipies():
+    return request.form

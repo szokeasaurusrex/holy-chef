1f97ef3c635ddd216d0cffea79b15c35223b314a
willtbowling <94068490+willtbowling@users.noreply.github.com>
2023-02-07 17:14:08 -0500

    Add Module docstring and Function docstring

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+"""Import to create Flask UI"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    """Hello word"""
     return render_template('index.html')

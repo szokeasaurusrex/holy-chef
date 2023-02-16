65a29524667b9dd54f8ead801c8529115dba9427
Chris <chrisyao2@gmail.com>
2023-02-16 16:51:05 -0500

    Additional Functions

--- python/elastic_manager.py
+++ python/elastic_manager.py
@@ -1,14 +1,10 @@
 from elasticsearch import Elasticsearch
-from elasticsearch.client import IndicesClient
+"""from elasticsearch.client import IndicesClient"""
 
 class Elastic_Manager:
-    
     def __init__(self, port):
-        self.es_client = Elasticsearch("http://localhost:9200")
-    
-    def retrieveRecipe(self, recipeInfopush):
+        self.es_client = Elasticsearch(port)
+    def retrieve_recipe(self, recipe_infopush):
         print("Hello")
-
-    def insertDocument(self, indexName, documentJSON):
-        self.es_client.index(index=indexName, document=documentJSON)
-        
+    def insert_document(self, index_name, document_JSON):
+        self.es_client.index(index=index_name, document=document_JSON)
--- python/server_main.py
+++ python/server_main.py
@@ -1,11 +1,11 @@
 import elastic_manager
-from elastic_manager import Elastic_Manager
+from elastic_manager import ElasticManager
 
 def main():
     """main method"""
     port = "http://localhost:9200"
-    manager = Elastic_Manager(port)
+    manager = ElasticManager(port)
     manager.insertDocument("")
 
 if __name__ == "__main__":
-     main()
+    main()

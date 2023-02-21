a7693a28e5c31f66819263d4ec2082e8bf1e1ee2
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2023-02-20 19:04:42 -0500

    Searching for recipes fully implemented

--- python/elastic_manager.py
+++ python/elastic_manager.py
@@ -1,6 +1,6 @@
+import os
 from elasticsearch import Elasticsearch
 from dotenv import load_dotenv
-import os
 
 class ElasticManager:
     """ElasticManager"""
@@ -8,12 +8,8 @@ class ElasticManager:
         load_dotenv()
         self.es_client = Elasticsearch(port, verify_certs=False, basic_auth=('elastic', os.environ['elastic_password']))
 
-    def retrieve_recipe(self, ingredient_array, time_cook):
+    def retrieve_recipe(self, ingredient_string, time_cook):
         """retrieve_recipe"""
-
-        ingredient_string = self.ingredients_to_string(ingredient_array)
-        
-
         query_body = {
             "bool":
             {
@@ -35,22 +31,8 @@ class ElasticManager:
                 ]
             }
         }
-        result = self.es_client.search(index="recipes", query=query_body)
-        print("Result ", result["hits"]["hits"])
-        return result
-
-    def ingredients_to_string(self, ingredient_array):
-        """ingredientsToString"""
-        ingredient_string = ' '
-
-        for ingredient in ingredient_array:
-            if ingredient_string == ' ':
-                ingredient_string += ingredient
-            else:
-                ingredient_string += "," + ingredient
-
-        return ingredient_string
-
+        results = self.es_client.search(index="recipes", query=query_body)
+        return [result['_source'] for result in results['hits']['hits']]
 
     def insert_document(self, index_name, document_json):
         """insert_document"""

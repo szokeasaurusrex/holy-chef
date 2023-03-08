6469d145fc48317f41a5b73628d1af4c1223d61f
willtbowling <wbowling13@gmail.com>
2023-03-08 16:04:49 -0500

    Updated recipe scrapping code

--- python/url_getter.py
+++ python/url_getter.py
@@ -4,7 +4,8 @@ import re  # pip install regex
 f = open('allrecipe_source.txt', 'r', encoding='UTF-8')
 contents = f.read()
 url = re.findall(
-    r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)", contents)
+    r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]"
+    r"{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)", contents)
 with open("recipe_url.csv", 'w', newline='', encoding='UTF-8') as csvfile:
 
     # writer = csv.writer(rec)

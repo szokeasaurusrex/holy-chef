b77a176e6860f908eeb12ec822b6bb8c262f92c4
Chris <chrisyao2@gmail.com>
2023-04-19 22:51:33 -0400

    Pylint Fixes

--- src/python/recipe_merge.py
+++ src/python/recipe_merge.py
@@ -18,14 +18,16 @@ class MergeRecipes:
     def generate_merged_recipe(self, recipelist1, recipelist2):
         """generate_merged_recipe"""
         chat_gpt_manager = ChatGPTManager()
-        system_prompt = """The user will provide you the title and ingredients of two recipes.Your job is to invent a never-before-seen recipe inspired by the two given recipes.
+        
+        # pylint: disable=line-too-long
+        system_prompt = """The user will provide you the title and ingredients of two recipes.Your job is to invent a never-before-seen recipe inspired by the two given recipes. 
         Please output the recipe you invent in JSON format. The JSON should include the following fields: 
         -"title": The recipe title, as a string
         - "ingredients": The ingredients, as an array of strings, with each string containing one ingredient
         - "instructions": The instructions, as an array of strings, 
-        with each string containing one step for the recipe """
+        with each string containing one step for the recipe """ 
         print(system_prompt)
-        user_input = "Recipe 1. Title: " + recipelist1['title'] + "." + "Ingredients: " + recipelist1['ingredients'] +  "Recipe 2. Title: " + recipelist2['title'] + "." + "Ingredients: " + recipelist2['ingredients'] # pylint: disable=unsubscriptable-object
+        user_input = "Recipe 1. Title: " + recipelist1['title'] + "." + "Ingredients: " + recipelist1['ingredients'] +  "Recipe 2. Title: " + recipelist2['title'] + "." + "Ingredients: " + recipelist2['ingredients'] # pylint: disable=unsubscriptable-object,disable=line-too-long
 
         result = chat_gpt_manager.text_completion(system_prompt,user_input)
         return result
